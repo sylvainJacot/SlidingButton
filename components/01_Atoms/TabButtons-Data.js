27c10e072ece354c58edb9e67b9f30baa0370caf
coatriver <sylvain.jacot@hotmail.be>
2020-10-28 16:55:07 +0100

    Fourth button label changed

--- components/01_Atoms/TabButtons-Data.js
+++ components/01_Atoms/TabButtons-Data.js
@@ -1,22 +1,22 @@
 export const TabButtonsItems = [
-    {
-      id: 1,
-      label: "Wake up",
-      isActive: false,
-    },
-    {
-      id: 2,
-      label: "Coffee",
-      isActive: false,
-    },
-    {
-      id: 3,
-      label: "Work",
-      isActive: false,
-    },
-    {
-      id: 4,
-      label: "Die",
-      isActive: false,
-    },
-  ];
+  {
+    id: 1,
+    label: "Wake up",
+    isActive: false,
+  },
+  {
+    id: 2,
+    label: "Coffee",
+    isActive: false,
+  },
+  {
+    id: 3,
+    label: "Work",
+    isActive: false,
+  },
+  {
+    id: 4,
+    label: "Stay Awake",
+    isActive: false,
+  },
+];
